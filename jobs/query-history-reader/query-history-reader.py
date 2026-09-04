1a5be930c4e0c69cb061887ab9457bd53b32d465
ziwikiwi-Isengard <ziwikiwi-Isengard>
2026-09-04 21:24:14 +0000

    Add Drop columns transform to drop duration_ms

--- jobs/query-history-reader/query-history-reader.py
+++ jobs/query-history-reader/query-history-reader.py
@@ -6,4 +6,6 @@ sc = SparkContext.getOrCreate()
 spark = SparkSession.builder.getOrCreate()
 
 # Script generated for node S3DataSource
-S3DataSource_node_s3_source = spark.read.format("parquet").load("s3://amazon-sagemaker-027024089510-us-west-2-635znel7hip47k/shared/query-history/event_date=2026-08-29/")
+S3DataSource_17885566296935 = spark.read.format("parquet").load("s3://amazon-sagemaker-027024089510-us-west-2-635znel7hip47k/shared/query-history/event_date=2026-08-29/")
+# Script generated for node DropTransform
+DropTransform_17885566296936 = S3DataSource_17885566296935.drop("duration_ms")
