0bc6e3d24f9bf28d4bb2fac6f99a0d4ccd5ca753
ziwikiwi-Isengard <ziwikiwi-Isengard>
2026-09-10 16:08:43 +0000

    Add VETL job my-workflows-3-to-s3-2

---
+++ jobs/my-workflows-3-to-s3-2/my-workflows-3-to-s3-2.py
@@ -0,0 +1,18 @@
+import sys
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+
+
+sc = SparkContext.getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+
+
+# Script generated for node S3DataSource
+S3DataSource_node_s3_source = spark.read.format("text").load("s3://amazon-sagemaker-027024089510-us-west-2-635znel7hip47k/shared/workflows/my-workflows-3.yaml")
+
+
+# Script generated for node S3DataSink
+S3DataSource_node_s3_source.write.format("csv") \
+    .option("header", True) \
+    .mode("append") \
+    .save("s3://sagemaker-glue-output-027024089510/my-workflows-3-2-output/")
